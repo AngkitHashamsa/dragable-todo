f2852f5d7eaf7efb6d3915a2ded9a1cfaee4661b
Angkit <angkithashamsa21@gmail.com>
2022-03-01 00:37:01 +0530

    added side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,61 @@
 import { useState } from "react";
 import InputFiled from "./components/InputFiled";
+
 import { Todo } from "./Interface/inputInterface";
 import Todos from "./components/Todos";
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
-
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [selectedId, setSelectedId] = useState<string>("");
+  const [editValue, setEditValue] = useState<string>("");
+  const [check, setCheck] = useState<boolean>(false);
   const handleSubmit = (e: React.FormEvent): void => {
     e.preventDefault();
-    console.log("submit");
-    let newTodo: Todo = {
-      id: new Date().getTime().toString(),
-      isDone: false,
-      todo,
-    };
-    setTodos([...todos, newTodo]);
+    // console.log("submit");
+    if (isEdit) {
+      // console.log("isEdit");
+      let newData = todos.map((item) => {
+        if (item.id === selectedId) {
+          return { ...item, todo: editValue, isDone: check ? true : false };
+        }
+        return item;
+      });
+      // console.log(newData);
+
+      setTodos(newData);
+      setIsEdit(false);
+    }
+    if (!isEdit) {
+      let newTodo: Todo = {
+        id: new Date().getTime().toString(),
+        isDone: false,
+        todo,
+      };
+      setTodos([...todos, newTodo]);
+    }
+
     setTodo("");
+  };
+
+  const handleUpdate = (id: string): void => {
+    // console.log(id);
+    let singleTodo = todos.find((item) => item.id === id);
+    // console.log(singleTodo);
+    if (singleTodo) {
+      setEditValue(singleTodo.todo);
+    }
+    setIsEdit(true);
+    setSelectedId(id);
+  };
+
+  const handleDelete = (id: string): void => {
+    let newTodo = todos.filter((item) => item.id !== id);
+    setTodos(newTodo);
+  };
+  const cancelEdit = (): void => {
+    setIsEdit(false);
+    setSelectedId("");
   };
   return (
     <div className="text-gray-600">
@@ -30,8 +70,21 @@ const App: React.FC = () => {
             setTodo={setTodo}
           />
         </section>
-        <section className="mx-auto w-[95%] max-w-xl ">
-          <Todos todos={todos} />
+        <section className="mx-auto my-4 mt-3 w-[95%] max-w-xl ">
+          <h4 className="">Todos</h4>
+          <Todos
+            todos={todos}
+            handleDelete={handleDelete}
+            handleUpdate={handleUpdate}
+            isEdit={isEdit}
+            editValue={editValue}
+            setEditValue={setEditValue}
+            handleSubmit={handleSubmit}
+            cancelEdit={cancelEdit}
+            check={check}
+            setCheck={setCheck}
+            selectedId={selectedId}
+          />
         </section>
       </main>
     </div>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,19 +1,85 @@
 // import React from 'react'
 
 import { Todo } from "../Interface/inputInterface";
+import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
+import { FaTrashAlt } from "react-icons/fa";
+import { MdModeEditOutline } from "react-icons/md";
 interface props {
   todos: Todo[];
+  handleDelete: (id: string) => void;
+  handleUpdate: (id: string) => void;
+  isEdit: boolean;
+  editValue: string;
+  setEditValue: React.Dispatch<React.SetStateAction<string>>;
+  handleSubmit: (e: React.FormEvent) => void;
+  cancelEdit: () => void;
+  check: boolean;
+  setCheck: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedId: string;
 }
-const Todos: React.FC<props> = ({ todos }) => {
+const Todos: React.FC<props> = ({
+  todos,
+  handleDelete,
+  handleUpdate,
+  isEdit,
+  editValue,
+  setEditValue,
+  handleSubmit,
+  cancelEdit,
+  check,
+  setCheck,
+  selectedId,
+}) => {
   // console.log(todos);
 
   return (
-    <div className="mt-3 rounded-md bg-white px-3 py-2 shadow-md">
+    <div className=" h-[calc(100vh-5rem)] overflow-auto rounded-md bg-white px-3 py-4 ">
       {todos.map((item) => {
         return (
-          <article key={item.id} className="my-3 flex items-center  gap-2">
-            <h4>{item.todo}</h4>
-            <p>completed:{item.isDone}</p>
+          <article
+            key={item.id}
+            className="text-tea my-3 flex items-center justify-between gap-2 rounded-md bg-yellow-300 px-3  py-2"
+          >
+            <div>
+              {selectedId === item.id && isEdit ? (
+                <input
+                  value={editValue}
+                  onChange={(e) => setEditValue(e.target.value)}
+                  type="text"
+                  className="rounded-md px-2 py-1 text-base focus:outline-none"
+                />
+              ) : (
+                <>{item.isDone ? <h4>{item.todo}</h4> : <s>{item.todo}</s>}</>
+              )}
+              {/* <p>status : {item.isDone ? "True" : "False"}</p> */}
+            </div>
+            <div className="flex items-center gap-3">
+              {selectedId === item.id && isEdit ? (
+                <>
+                  <input
+                    type="checkbox"
+                    checked={check}
+                    onChange={(e) => setCheck(e.target.checked)}
+                  />
+                  <button onClick={handleSubmit}>
+                    <AiOutlineCheck className="text-2xl text-green-400" />
+                  </button>
+                </>
+              ) : (
+                <button onClick={() => handleUpdate(item.id)}>
+                  <MdModeEditOutline className="text-2xl text-gray-500" />
+                </button>
+              )}
+              {isEdit ? (
+                <button onClick={cancelEdit}>
+                  <AiOutlineClose className="text-2xl text-red-600" />
+                </button>
+              ) : (
+                <button onClick={() => handleDelete(item.id)}>
+                  <FaTrashAlt className="text-xl text-red-600" />
+                </button>
+              )}
+            </div>
           </article>
         );
       })}
